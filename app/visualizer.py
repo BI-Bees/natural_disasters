31aa1f0835f627eba821f4441ea6145475716c43
Michael <2750daugbjerg@gmail.com>
2018-11-24 13:12:28 +0100

    csv reader is working

--- app/visualizer.py
+++ app/visualizer.py
@@ -1,22 +1,30 @@
 from flask import Flask
 from flask import request
 import csv
+import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def visualize():
-	my_dict = read_csv()
-	return render_template('html_page.html', my_dict=my_dict)
+	result_list = read_csv()
+	return render_template('html_page.html', result_list=result_list)
 
+#Reading csv file and creates a list of dictionaries.
 def read_csv():
-	disaster_dict = {}
+	disaster_list = []
 	with open('disasters.csv') as csv_file:
-		line_reader = csv.DictReader(csv_file, delimiter=',')
-		for row in line_reader:
-			disaster_dict = int(row['year'])
-	print(disaster_dict)
-	return disaster_dict
+		for row in csv.DictReader(csv_file, skipinitialspace=True):
+			disaster_list.append({key: value for key, value in row.items()})
+	disaster_list = empty_space_filler(disaster_list)
+	print(disaster_list)
+	return disaster_list
+
+#Filling all empty values in a given list of dictionaries
+def empty_space_filler(custom_list):
+	for dicts in custom_list:
+		dicts.update((key, 0) for key, value in dicts.items() if not value)
+	return custom_list
 
 if __name__ == '__main__':
 	read_csv()
